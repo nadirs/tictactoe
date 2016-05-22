8ebcba1c3d2c759968ce587bf87643215ea83b8c
Nadir Sampaoli <nadirsampaoli@gmail.com>
2016-05-22 15:13:42 +0200

    Change members' visibility and refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,19 @@ use std::vec::IntoIter;
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-enum MoveMark { X, O }
+pub enum MoveMark { X, O }
 
 #[derive(Debug, PartialEq)]
-enum Player { Computer, Human }
+pub enum Player { Computer, Human }
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-enum HorizontalPos { Left, Center, Right }
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum HorizontalPos { Left, Center, Right }
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-enum VerticalPos { Top, Center, Bottom }
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum VerticalPos { Top, Center, Bottom }
 
 #[derive(Debug, PartialEq)]
-struct Board {
+pub struct Board {
     cells: HashMap<(HorizontalPos, VerticalPos), MoveMark>
 }
 
@@ -26,18 +26,18 @@ impl Board {
         }
     }
 
-    pub fn get_cell_at(&self, x: HorizontalPos, y: VerticalPos) -> Option<&MoveMark> {
+    fn get_cell_at(&self, x: HorizontalPos, y: VerticalPos) -> Option<&MoveMark> {
         self.cells.get(&(x, y))
     }
 
-    pub fn set_cell_at(&mut self, x: HorizontalPos, y: VerticalPos, mark: MoveMark) {
-        if let None = self.get_cell_at(x.clone(), y.clone()) {
+    fn set_cell_at(&mut self, x: HorizontalPos, y: VerticalPos, mark: MoveMark) {
+        if let None = self.get_cell_at(x, y) {
             self.cells.insert((x, y), mark);
         }
     }
 }
 
-struct Game {
+pub struct Game {
     player_1: Player,
     player_2: Player,
     board: Board,
@@ -56,106 +56,111 @@ impl Game {
         }
     }
 
-    pub fn turns() -> Cycle<IntoIter<MoveMark>> {
+    fn turns() -> Cycle<IntoIter<MoveMark>> {
         vec![MoveMark::X, MoveMark::O].into_iter().cycle()
     }
 
-    pub fn next_mark(&mut self) -> MoveMark {
+    fn next_mark(&mut self) -> MoveMark {
         self.turns.next().unwrap()
     }
 
-    pub fn fetch_move(&mut self) -> (BoardCell, MoveMark) {
+    fn fetch_move(&mut self) -> (BoardCell, MoveMark) {
         (0, self.next_mark())
     }
 }
 
-#[test]
-fn it_works() {
-    assert!(true)
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn default_computer_vs_computer() {
-    let game = Game::default();
-    assert_eq!(game.player_1, Player::Computer);
-    assert_eq!(game.player_2, Player::Computer);
-}
+    #[test]
+    fn it_works() {
+        assert!(true)
+    }
 
-#[test]
-fn starter_is_cross() {
-    let mut game = Game::default();
-    let (_, first_mark) = game.fetch_move();
-    assert_eq!(first_mark, MoveMark::X);
-}
+    #[test]
+    fn default_computer_vs_computer() {
+        let game = Game::default();
+        assert_eq!(game.player_1, Player::Computer);
+        assert_eq!(game.player_2, Player::Computer);
+    }
 
-#[test]
-fn second_is_nought() {
-    let mut game = Game::default();
-    let (_, _) = game.fetch_move();
-    let (_, second_mark) = game.fetch_move();
-    assert_eq!(second_mark, MoveMark::O);
-}
+    #[test]
+    fn starter_is_cross() {
+        let mut game = Game::default();
+        let (_, first_mark) = game.fetch_move();
+        assert_eq!(first_mark, MoveMark::X);
+    }
 
-#[test]
-fn a_board_cell_starts_empty() {
-    let game = Game::default();
-    let board = &game.board;
-    let cell = board.get_cell_at(HorizontalPos::Left, VerticalPos::Top);
-    assert_eq!(cell, None);
-}
+    #[test]
+    fn second_is_nought() {
+        let mut game = Game::default();
+        let (_, _) = game.fetch_move();
+        let (_, second_mark) = game.fetch_move();
+        assert_eq!(second_mark, MoveMark::O);
+    }
 
-#[test]
-fn can_mark_a_board_cell() {
-    let game = Game::default();
-    let mut board = game.board;
-    let mark = MoveMark::X;
-    board.set_cell_at(HorizontalPos::Center, VerticalPos::Center, mark);
-    let cell = board.get_cell_at(HorizontalPos::Center, VerticalPos::Center);
-    assert_eq!(cell, Some(&mark));
-}
+    #[test]
+    fn a_board_cell_starts_empty() {
+        let game = Game::default();
+        let board = &game.board;
+        let cell = board.get_cell_at(HorizontalPos::Left, VerticalPos::Top);
+        assert_eq!(cell, None);
+    }
 
-#[test]
-fn can_mark_two_board_cells_and_get_back_the_correct_marks() {
-    let game = Game::default();
-    let mut board = game.board;
-    board.set_cell_at(HorizontalPos::Center, VerticalPos::Center, MoveMark::X);
-    board.set_cell_at(HorizontalPos::Left, VerticalPos::Center, MoveMark::O);
-    let first_cell = board.get_cell_at(HorizontalPos::Center, VerticalPos::Center);
-    let second_cell = board.get_cell_at(HorizontalPos::Left, VerticalPos::Center);
-    assert_eq!(first_cell, Some(&MoveMark::X));
-    assert_eq!(second_cell, Some(&MoveMark::O));
-}
+    #[test]
+    fn can_mark_a_board_cell() {
+        let game = Game::default();
+        let mut board = game.board;
+        let mark = MoveMark::X;
+        board.set_cell_at(HorizontalPos::Center, VerticalPos::Center, mark);
+        let cell = board.get_cell_at(HorizontalPos::Center, VerticalPos::Center);
+        assert_eq!(cell, Some(&mark));
+    }
 
-#[test]
-fn can_mark_all_board_cells_and_get_back_the_correct_marks() {
-    let game = Game::default();
-    let mut board = game.board;
-    board.set_cell_at(HorizontalPos::Left, VerticalPos::Top, MoveMark::X);
-    board.set_cell_at(HorizontalPos::Left, VerticalPos::Center, MoveMark::O);
-    board.set_cell_at(HorizontalPos::Left, VerticalPos::Bottom, MoveMark::X);
-    board.set_cell_at(HorizontalPos::Center, VerticalPos::Top, MoveMark::X);
-    board.set_cell_at(HorizontalPos::Center, VerticalPos::Center, MoveMark::O);
-    board.set_cell_at(HorizontalPos::Center, VerticalPos::Bottom, MoveMark::X);
-    board.set_cell_at(HorizontalPos::Right, VerticalPos::Top, MoveMark::O);
-    board.set_cell_at(HorizontalPos::Right, VerticalPos::Center, MoveMark::X);
-    board.set_cell_at(HorizontalPos::Right, VerticalPos::Bottom, MoveMark::O);
+    #[test]
+    fn can_mark_two_board_cells_and_get_back_the_correct_marks() {
+        let game = Game::default();
+        let mut board = game.board;
+        board.set_cell_at(HorizontalPos::Center, VerticalPos::Center, MoveMark::X);
+        board.set_cell_at(HorizontalPos::Left, VerticalPos::Center, MoveMark::O);
+        let first_cell = board.get_cell_at(HorizontalPos::Center, VerticalPos::Center);
+        let second_cell = board.get_cell_at(HorizontalPos::Left, VerticalPos::Center);
+        assert_eq!(first_cell, Some(&MoveMark::X));
+        assert_eq!(second_cell, Some(&MoveMark::O));
+    }
 
-    assert_eq!(board.get_cell_at(HorizontalPos::Left, VerticalPos::Top), Some(&MoveMark::X));
-    assert_eq!(board.get_cell_at(HorizontalPos::Left, VerticalPos::Center), Some(&MoveMark::O));
-    assert_eq!(board.get_cell_at(HorizontalPos::Left, VerticalPos::Bottom), Some(&MoveMark::X));
-    assert_eq!(board.get_cell_at(HorizontalPos::Center, VerticalPos::Top), Some(&MoveMark::X));
-    assert_eq!(board.get_cell_at(HorizontalPos::Center, VerticalPos::Center), Some(&MoveMark::O));
-    assert_eq!(board.get_cell_at(HorizontalPos::Center, VerticalPos::Bottom), Some(&MoveMark::X));
-    assert_eq!(board.get_cell_at(HorizontalPos::Right, VerticalPos::Top), Some(&MoveMark::O));
-    assert_eq!(board.get_cell_at(HorizontalPos::Right, VerticalPos::Center), Some(&MoveMark::X));
-    assert_eq!(board.get_cell_at(HorizontalPos::Right, VerticalPos::Bottom), Some(&MoveMark::O));
-}
+    #[test]
+    fn can_mark_all_board_cells_and_get_back_the_correct_marks() {
+        let game = Game::default();
+        let mut board = game.board;
+        board.set_cell_at(HorizontalPos::Left, VerticalPos::Top, MoveMark::X);
+        board.set_cell_at(HorizontalPos::Left, VerticalPos::Center, MoveMark::O);
+        board.set_cell_at(HorizontalPos::Left, VerticalPos::Bottom, MoveMark::X);
+        board.set_cell_at(HorizontalPos::Center, VerticalPos::Top, MoveMark::X);
+        board.set_cell_at(HorizontalPos::Center, VerticalPos::Center, MoveMark::O);
+        board.set_cell_at(HorizontalPos::Center, VerticalPos::Bottom, MoveMark::X);
+        board.set_cell_at(HorizontalPos::Right, VerticalPos::Top, MoveMark::O);
+        board.set_cell_at(HorizontalPos::Right, VerticalPos::Center, MoveMark::X);
+        board.set_cell_at(HorizontalPos::Right, VerticalPos::Bottom, MoveMark::O);
 
-#[test]
-fn cannot_overwrite_a_marked_cell() {
-    let game = Game::default();
-    let mut board = game.board;
-    board.set_cell_at(HorizontalPos::Left, VerticalPos::Top, MoveMark::X);
-    board.set_cell_at(HorizontalPos::Left, VerticalPos::Top, MoveMark::O);
-    assert_eq!(board.get_cell_at(HorizontalPos::Left, VerticalPos::Top), Some(&MoveMark::X));
+        assert_eq!(board.get_cell_at(HorizontalPos::Left, VerticalPos::Top), Some(&MoveMark::X));
+        assert_eq!(board.get_cell_at(HorizontalPos::Left, VerticalPos::Center), Some(&MoveMark::O));
+        assert_eq!(board.get_cell_at(HorizontalPos::Left, VerticalPos::Bottom), Some(&MoveMark::X));
+        assert_eq!(board.get_cell_at(HorizontalPos::Center, VerticalPos::Top), Some(&MoveMark::X));
+        assert_eq!(board.get_cell_at(HorizontalPos::Center, VerticalPos::Center), Some(&MoveMark::O));
+        assert_eq!(board.get_cell_at(HorizontalPos::Center, VerticalPos::Bottom), Some(&MoveMark::X));
+        assert_eq!(board.get_cell_at(HorizontalPos::Right, VerticalPos::Top), Some(&MoveMark::O));
+        assert_eq!(board.get_cell_at(HorizontalPos::Right, VerticalPos::Center), Some(&MoveMark::X));
+        assert_eq!(board.get_cell_at(HorizontalPos::Right, VerticalPos::Bottom), Some(&MoveMark::O));
+    }
+
+    #[test]
+    fn cannot_overwrite_a_marked_cell() {
+        let game = Game::default();
+        let mut board = game.board;
+        board.set_cell_at(HorizontalPos::Left, VerticalPos::Top, MoveMark::X);
+        board.set_cell_at(HorizontalPos::Left, VerticalPos::Top, MoveMark::O);
+        assert_eq!(board.get_cell_at(HorizontalPos::Left, VerticalPos::Top), Some(&MoveMark::X));
+    }
 }
